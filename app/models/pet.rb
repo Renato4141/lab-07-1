eb52f716b4c23fc96b7c08321e62e02f2c9ef5ed
Renato4141 <rfilzauspe@miuandes.cl>
2026-05-07 09:24:15 -0400

    some shows and forms going around

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :owner
   has_many :appointments, dependent: :destroy
+  has_one_attached :photo
 
   def date_not_in_future
     if date_of_birth.present? && date_of_birth > Date.today
@@ -28,4 +29,16 @@ class Pet < ApplicationRecord
   validates :weight, presence: true, numericality: { greater_than: 0 }
 
   scope :by_species, ->(species) { where(species: species.to_s.downcase) }
+
+  def photo_validation
+    return unless photo.attached?
+     allowed_types = ["image/jpeg", "image/png", "image/webp"]
+
+    unless allowed_types.include?(photo.content_type)
+      errors.add(:photo, "must be a JPEG, PNG, or WEBP type")
+    end
+
+  if photo.bytesize > 5.megabytes
+    errors.add(:photo, "Maximum size is 5MB.")
+  end
 end
